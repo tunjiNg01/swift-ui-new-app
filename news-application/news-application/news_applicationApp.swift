49e79846cefb5130f1b83bbe161ee4a990b3d4c4
tunjiNg01 <nahtlosng@gmail.com>
2023-09-02 13:45:31 +0100

    news app init

---
+++ news-application/news-application/news_applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_applicationApp.swift
+//  news-application
+//
+//  Created by MACBOOK PRO on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct news_applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
